4711b00988548d4e6377b6ad4642f76a599bc654
Lisa Tassone <lisa.tassone@cultureamp.com>
2024-01-04 08:57:05 +1000

    broken

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -16,16 +16,16 @@ describe('API Pact test', () => {
   describe('retrieving a product', () => {
     test('ID 10 exists', async () => {
       // Arrange
-      const expectedProduct = {
-        id: '10',
-        type: 'CREDIT_CARD',
-        name: '28 Degrees',
-        color: 'red',
-        price: 'n/a'
-      };
+      // const expectedProduct = {
+      //   id: '10',
+      //   type: 'CREDIT_CARD',
+      //   name: '28 Degrees',
+      //   color: 'red',
+      //   price: 'n/a'
+      // };
 
       // Uncomment to see this fail
-      // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
 
       mockProvider
         .given('a product with ID 10 exists')
