a356e32b76b7f1de2575e36b4ac9105d58f63943
Lisa Tassone <lisa.tassone@cultureamp.com>
2024-01-03 15:25:37 +1000

    add price

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -20,7 +20,8 @@ describe('API Pact test', () => {
         id: '10',
         type: 'CREDIT_CARD',
         name: '28 Degrees',
-        color: 'red'
+        color: 'red',
+        price: 'n/a'
       };
 
       // Uncomment to see this fail
