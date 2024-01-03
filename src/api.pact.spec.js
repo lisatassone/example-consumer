1f2fe6286aceaace41e95d396d9745076ebaf4f0
Lisa Tassone <lisa.tassone@cultureamp.com>
2024-01-03 16:48:45 +1000

    remove name

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -19,8 +19,8 @@ describe('API Pact test', () => {
       const expectedProduct = {
         id: '10',
         type: 'CREDIT_CARD',
-        name: '28 Degrees',
-        color: 'red'
+        color: 'red',
+        price: 'n/a'
       };
 
       // Uncomment to see this fail
